d600b69455100540343f0467353bb88a0c0afb7e
namuan <575441+namuan@users.noreply.github.com>
2020-01-07 16:31:36 +0000

    [Added] Create new diagram with a unique identifier

---
+++ src/main/java/dev/deskriders/sketchrider/api/DiagramController.java
@@ -0,0 +1,32 @@
+package dev.deskriders.sketchrider.api;
+
+import io.micronaut.core.util.CollectionUtils;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
+import io.micronaut.views.View;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+import java.util.UUID;
+
+@Controller
+@Secured(SecurityRule.IS_ANONYMOUS)
+public class DiagramController {
+
+    @Get(value = "/diagrams/new", produces = MediaType.TEXT_HTML)
+    public HttpResponse createNewDiagram() throws URISyntaxException {
+        String diagramId = UUID.randomUUID().toString();
+        return HttpResponse.redirect(new URI("/diagrams/" + diagramId));
+    }
+
+    @View(value = "/diagrams/edit")
+    @Get(value = "/diagrams/{diagramId}", produces = MediaType.TEXT_HTML)
+    public Map<String, Object> diagram(String diagramId) {
+        return CollectionUtils.mapOf("diagramId", diagramId);
+    }
+}
--- src/main/java/dev/deskriders/sketchrider/api/HomeController.java
+++ src/main/java/dev/deskriders/sketchrider/api/HomeController.java
@@ -1,6 +1,6 @@
 package dev.deskriders.sketchrider.api;
 
-import io.micronaut.core.util.CollectionUtils;
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -10,8 +10,6 @@ import io.micronaut.security.rules.SecurityRule;
 import io.micronaut.views.View;
 
 import javax.annotation.Nullable;
-import java.util.Collections;
-import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -19,16 +17,7 @@ public class HomeController {
     @Secured(SecurityRule.IS_ANONYMOUS)
     @View(value = "index")
     @Get(produces = MediaType.TEXT_HTML)
-    public Map index(@Nullable Authentication authentication) {
-        if (authentication == null) {
-            return Collections.singletonMap("loggedIn", false);
-        }
-
-        System.out.println(authentication.getAttributes());
-        return CollectionUtils.mapOf(
-                "loggedIn", true,
-                "username", authentication.getName(),
-                "uid", authentication.getAttributes().get("id")
-        );
+    public HttpResponse index(@Nullable Authentication authentication) {
+        return HttpResponse.ok();
     }
 }
